0b97ee4c6943d3aa8e0fd226ca575cb6332d8510
JOJOK63 <77489999+JOJOK63@users.noreply.github.com>
2025-06-09 12:10:22 +0200

    starter

---
+++ src/main/java/com/app/toolsUneedBack/ToolsUneedBackApplication.java
@@ -0,0 +1,13 @@
+package com.app.toolsUneedBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsUneedBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsUneedBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/toolsUneedBack/ToolsUneedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.toolsUneedBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsUneedBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
